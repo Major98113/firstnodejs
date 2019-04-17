0e8c6b80c65540af7a1a5172bf0f97c2947b3dfc
max <meison98@bk.ru>
2019-04-17 17:53:47 +0300

    Есть проблемы с установкой Zombie

--- meadowlark.js
+++ meadowlark.js
@@ -29,6 +29,15 @@ app.get('/about', function(req, res) {
         pageTestScript: '/qa/tests-about.js'
     });
 });
+
+app.get('/tours/hood-river', function(req, res){
+    res.render('tours/hood-river');
+});
+
+app.get('/tours/request-group-rate', function(req, res){
+    res.render('tours/request-group-rate');
+});
+
 // Обобщенный обработчик 404 (промежуточное ПО)
 app.use(function(req, res, next){
     res.status(404);
